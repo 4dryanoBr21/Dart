047a3e64a34983d6dc98ad52b9eccab036c6bc4a
4dryanoBr21 <adrianocanto462@gmail.com>
2025-08-26 14:26:40 -0300

    Tarefa2

--- main.dart
+++ main.dart
@@ -19,6 +19,7 @@ var n2 = 3;
 var soma = 3 + 2;
 */
 
+/*
 void main() {
   print(soma);
 }
@@ -27,3 +28,35 @@ var n1 = 2;
 var n2 = 3;
 
 var soma = "3 + 2";
+*/
+
+/*
+void main() {
+  print(str);
+  print(numero_int);
+  print(numero_dacimal);
+}
+
+var str = "Ola, mundo!";
+int numero_int = 10;
+double numero_dacimal = 9.5;
+*/
+
+/*
+void main() {
+  print(str);
+  print(numero_int);
+  print(numero_dacimal);
+}
+
+var str = "Ola, mundo!";
+double numero_int = 10.1;
+double numero_dacimal = 9.5;
+*/
+
+void main() {
+  print(ano);
+}
+
+var ano = DateTime.now().year;
+var test = ano + 1;
