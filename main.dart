6a156659d2b5849ca0bc9ae0bdf09563ade60e9f
4dryanoBr21 <adrianocanto462@gmail.com>
2025-08-26 14:10:11 -0300

    main

---
+++ main.dart
@@ -0,0 +1,29 @@
+/*
+void main() {
+  print("Meu nome é Adriano");
+  print("Estou aprendendo Dart!");
+  print("Esse é o ultimo dia!");
+  print("Dart é divertido");
+  print('Dart também aceita aspas simples');
+}
+*/
+
+/*
+void main() {
+  print(soma);
+}
+
+var n1 = 2;
+var n2 = 3;
+
+var soma = 3 + 2;
+*/
+
+void main() {
+  print(soma);
+}
+
+var n1 = 2;
+var n2 = 3;
+
+var soma = "3 + 2";
