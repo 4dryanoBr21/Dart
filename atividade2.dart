19a1e9a0db6a090bbf8a27d7e80674d7440b0460
4dryanoBr21 <adrianocanto462@gmail.com>
2025-08-26 15:00:03 -0300

    Atualização dos arquivos

---
+++ atividade2.dart
@@ -0,0 +1,57 @@
+//ATIVIDADE 2
+
+/*
+void main() {
+  print(str);
+  print(numero_int);
+  print(numero_dacimal);
+}
+
+var str = "Ola, mundo!";
+int numero_int = 10;
+double numero_dacimal = 9.5;
+*/
+
+/*
+void main() {
+  print(str);
+  print(numero_int);
+  print(numero_dacimal);
+}
+
+var str = "Ola, mundo!";
+int numero_int = 10.1;
+double numero_dacimal = 9.5;
+*/
+
+/*
+void main() {
+  ano = ano + 1;
+  print(ano);
+}
+
+final ano = DateTime.now().year;
+*/
+
+/*
+void main() {
+  print(desconto);
+}
+
+const taxa = 0.1;
+double produto = 34;
+
+var desconto = taxa * produto;
+*/
+
+/*
+void main() {
+  print(nome);
+  print(nota);
+  print(PI);
+}
+
+const nome = "Ferdinando";
+var nota = 8;
+final PI = 9;
+*/
