5e424c3372e374f18b4d0d6d039792cf1fc3d68b
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-02 15:13:57 -0300

    test

--- atividade3.dart
+++ atividade3.dart
@@ -1,5 +1,4 @@
-void main(){
-
+void main() {
   /*int valor = 0;
 
   if (valor % 2 == 0) {
@@ -11,6 +10,19 @@ void main(){
   /*var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   for (var n in num) {
     print("Número $n");
+  }*/
+
+  /*var frutas = ["Maçã", "Pera", "Banana"];
+
+  for (var fruta in frutas) {
+    print("Eu gosto de $fruta");
+  }*/
+
+  /*int contador = 0;
+
+  while (contador <= 5) {
+    print(contador);
+    contador ++;
   }*/
 
   
