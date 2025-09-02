aadc1e40ed7de3fd3e8805ee6b1bcd2b4e8522cc
4dryanoBr21 <adrianocanto462@gmail.com>
2025-09-02 14:37:03 -0300

    for int

---
+++ atividade3.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  /*int valor = 0;
+
+  if (valor % 2 == 0) {
+    print("O valor é par");
+  } else {
+    print("O valor não tem par hahaha");
+  }*/
+
+  /*var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (var n in num) {
+    print("Número $n");
+  }*/
+
+  
+
+}
